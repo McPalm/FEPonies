e4cd8fa00c122373f0539dbab525bcc6cf581601
Palm <tauman.exe@gmail.com>
2016-09-15 10:29:07 +0200

    Added AttackBuff.cs (interface)

---
+++ Assets/Src/BuffsAndAuras/AttackBuff.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Registered on the Unit with the AttackBuff.
+/// Call unit.RegisterAttackBuff(this);
+/// </summary>
+public interface AttackBuff{
+
+	/// <summary>
+	/// Find out whenever or not the attack applies
+	/// This needs to be consistent, no random variables in this function.
+	/// </summary>
+	/// <param name="target"></param>
+	/// <param name="where the unit stands when making the attack."></param>
+	/// <returns></returns>
+	bool Applies(Unit target, Tile source);
+
+	/// <summary>
+	/// The stat buff if it applies.
+	/// </summary>
+	Stats Stats{
+		get;
+	}
+
+}
