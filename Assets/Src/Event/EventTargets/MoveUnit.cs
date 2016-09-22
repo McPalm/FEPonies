87c3170126fb2c68df23ceecf9083b065a305925
Peter Åkesson <petak86@gmail.com>
2016-09-22 17:18:52 +0200

    Wrote in all the dialogue for the first stage into the game. Fixed up all the starting and ending events for the first stage. The MoveUnit event can now trigger new events.

--- Assets/Src/Event/EventTargets/MoveUnit.cs
+++ Assets/Src/Event/EventTargets/MoveUnit.cs
@@ -1,13 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
 
 public class MoveUnit : EventTarget{
 
 	public Unit moveMe;
 	public Tile moveTo;
+    public bool active=false;
+    public List<EventTarget> events;
 
-	public override void Notice()
+    private bool callback;
+
+    public override void Notice()
 	{
 		moveMe.MoveToAndAnimate(moveTo, true);
-	}
+        active = true;
+    }
+
+    public void Update()
+    {
+        if(active&&StateManager.Instance.Peek()!=GameState.unitMoving)
+        {
+            CallBack();
+            active = false; 
+        }
+    }
+
+    public void CallBack()
+    {
+        foreach (EventTarget et in events)
+        {
+            try
+            {
+                et.Notice();
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError(e);
+                Debug.Log(et);
+            }
+        }
+    }
 }
