ec8be97adde5ccd6cf4318d198396e3d10e4c481
Peter Åkesson <petak86@gmail.com>
2016-09-30 13:43:32 +0200

    Added a levelDB class to handle string loading for levels, and various other things maybe.

---
+++ Assets/Src/LevelDB.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class LevelDB
+{
+    private string[] levels={
+        "mainMenu",
+        "RoadAmbush",
+        "BarbarianDefence"
+        };
+    public string getMainMenu()
+    {
+        return levels[0];
+    }
+
+    public string getFirstLevel()
+    {
+        return levels[1];
+    }
+
+    public List<string> getAllLevels()
+    {
+        List<string> temp = new List<string>(levels);
+        return temp;
+    }
+
+    public string getNextLevel(string curr)
+    {
+        for(int n=0;n<levels.Length;n++)
+        {
+            if (n==levels.Length-1)
+            {
+                Debug.Log("Last Level");
+                return ("-1");
+            }
+            if (levels[n]==curr)
+            {
+                return levels[n + 1];
+            }
+        }
+        Debug.Log("Level not Found");
+        return ("-2");
+    }
+}
