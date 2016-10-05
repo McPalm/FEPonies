d6c522accce4959133d3d50ac35ce7c8cef5f490
Peter Åkesson <petak86@gmail.com>
2016-10-05 15:44:41 +0200

    Saving and loading to files added. Saves characterlevel, and backpack to a defined or default file.

---
+++ Assets/Src/Items/ItemString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public static class ItemString
+{
+    /// <summary>
+    /// Takes an itemname a string and returns an Item
+    /// </summary>
+    /// <param name="itemName">The string that is to be translated</param>
+    /// <returns></returns>
+    public static Item StringToItem(string itemName)
+    {
+        //TODO actually making this thing translate strings to items
+        Item rv = new HealingPotion();
+        rv.Name = "TestPotion";
+        return rv;
+    }
+}
