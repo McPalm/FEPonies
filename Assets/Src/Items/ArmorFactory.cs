bad3c79e2117e9ed459b46f67b843ff843794cdb
Palm <tauman.exe@gmail.com>
2016-10-20 12:08:47 +0200

    Bugswatted ArmorFactory

--- Assets/Src/Items/ArmorFactory.cs
+++ Assets/Src/Items/ArmorFactory.cs
@@ -8,6 +8,10 @@ public class ArmorFactory
 	int weight = 0;
 	float resistance = 0.5f;
 	string name;
+	bool agility = false;
+	bool intelligence = false;
+	bool dexterity = false;
+	bool strenght = false;
 
 	public ArmorFactory(string name = "")
 	{
@@ -51,9 +55,28 @@ public class ArmorFactory
 		set
 		{
 			if (value < 0) Debug.LogError("armour wiehgt cannot be less than 0");
-			else if(value > 3) Debug.LogError("armour wiehgt cannot more than 3");
 			else weight = value;
 		}
+	}
+
+	public void AgilityBonus()
+	{
+		agility = true;
+	}
+
+	public void StrenghtBonus()
+	{
+		strenght = true;
+	}
+
+	public void IntelligenceBonus()
+	{
+		intelligence = true;
+	}
+
+	public void DexterityBonus()
+	{
+		dexterity = true;
 	}
 
 	public Armor GetArmour()
@@ -61,23 +84,41 @@ public class ArmorFactory
 		float power = level + 5;
 		float advantages = 0f;
 		float disadvantages = 0f;
+		Debug.Log("Base Power: " + power);
+		
+		// count advantages/disadvantages
+		disadvantages += weight / 20f;
 
-		disadvantages += weight / 4f;
+		if (strenght) advantages += 0.25f;
+		if (dexterity) advantages += 0.25f;
+		if (agility) advantages += 0.25f;
+		if (intelligence) advantages += 0.25f;
 
+		power /= 1f + advantages;
+		power *= 1f + disadvantages;
+
+		// set base stats
 		Armor a = new Armor();
 		a.weight = weight;
 		a.buff = new Stats();
 		a.Name = name;
+		Debug.Log("Power: " + power);
+		Debug.Log("Reistance: " + resistance);
+		Debug.Log("Weight: " + weight);
 
-		power /= 1f + advantages;
-		power *= 1f + disadvantages;
-		a.buff.defense = Mathf.RoundToInt(power * (1f - resistance) / 1.5f) + weight;
-		a.buff.defense = Mathf.RoundToInt(power * resistance / 1.5f);
+		a.buff.defense = Mathf.RoundToInt(power * 0.99f* (1f - resistance) + weight * 0.25f);
+		a.buff.resistance = Mathf.RoundToInt(power * 1.01f * resistance);
 
+		// apply special attributes
+		if (strenght) a.buff.strength = 1 + (int)(power / 4f);
+		if (dexterity) a.buff.dexterity = 1 + (int)(power / 4f);
+		if (agility) a.buff.agility = 1 + (int)(power / 4f);
+		if (intelligence) a.buff.intelligence = 1 + (int)(power / 4f);
 
-
+		// calculate value
 		level += 5;
-		a.value = (int)((level * level) * (0.5f + weight));
+		a.value = (int)((level * power) * (0.5f + weight * 0.1f));
+
 		return a;
 	}
 }
