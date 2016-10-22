fe4a29ffc7de3efb7b86f7120a265df4c1d1f7a3
Palm <tauman.exe@gmail.com>
2016-10-22 11:29:11 +0200

    Old items no longer linger in the Item Frame

--- Assets/Src/GUI/ItemFrame.cs
+++ Assets/Src/GUI/ItemFrame.cs
@@ -5,26 +5,58 @@ public class ItemFrame :  ScrollMenu {
 
 	[SerializeField]
 	ItemInfo template;
+
+	List<ItemInfo> ilist;
+	int count = 0;
 	
 	void Awake()
 	{
-		template.gameObject.SetActive(false);
+		ilist = new List<ItemInfo>();
+		ilist.Add(template);
+		Clear();
 	}
 	
 	public void Build(Backpack b)
 	{
+		Clear();
 		int n = 0;
 		foreach(Item i in b)
 		{
-			ItemInfo ii = Instantiate<ItemInfo>(template);
-			ii.transform.SetParent(anchor);
+			ItemInfo ii = GetNext();
 			ii.Build(i);
 			ii.transform.localPosition = new Vector3(0f, + 65 - n * 50 + 0);
-			ii.transform.localScale = Vector3.one;
 			ii.gameObject.SetActive(true);
 			ii.Equipped = (i is Equipment && b.IsEquipped((Equipment)i));
 			n++;
 		}
 		Height = n * 50;
+	}
+
+	ItemInfo GetNext()
+	{
+		count++;
+		if (count > ilist.Count)  // generate new button
+		{
+			Debug.Log("generating button");
+			ItemInfo ii = Instantiate<ItemInfo>(template);
+			ii.transform.SetParent(anchor);
+			ii.transform.localScale = Vector3.one;
+			count++;
+			ilist.Add(ii);
+			return ii;
+		}
+		
+		return ilist[count-1];
+	}
+
+	public void Clear()
+	{
+		Height = 0;
+		foreach(ItemInfo ii in ilist)
+		{
+			ii.gameObject.SetActive(false);
+			ii.transform.localScale = Vector3.one;
+		}
+		count = 0;
 	}
 }
