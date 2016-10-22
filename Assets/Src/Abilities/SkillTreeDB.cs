65b52c7fa8e706ca28605d67fd38f7538551ba04
Palm <tauman.exe@gmail.com>
2016-10-22 13:29:55 +0200

    Created a Database for SkillTrees

---
+++ Assets/Src/Abilities/SkillTreeDB.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SkillTreeDB : MonoBehaviour {
+
+	static SkillTreeDB instance;
+	static SkillTreeDB Instance
+	{
+		get
+		{
+			if (instance == null) instance = Resources.Load<SkillTreeDB>("SkilltreeDB");
+			return instance;
+		}
+	}
+
+	[SerializeField]
+	List<TreeContainer> trees;
+
+	static SkillTree GetSkillTree(string name)
+	{
+		foreach(TreeContainer st in Instance.trees)
+		{
+			if (st.name == name)
+				return st.tree;
+		}
+		Debug.LogError(name + " not found in the skilltree database");
+		return null;
+	}
+
+	[System.Serializable]
+	private class TreeContainer
+	{
+		public string name;
+		public SkillTree tree;
+	}
+}
