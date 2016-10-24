2d11a6ac6d3f52e93f020b4cf90bf2c4c4d6c2b7
Palm <tauman.exe@gmail.com>
2016-10-24 12:37:55 +0200

    Updated Main Menu

---
+++ Assets/Src/GUI/MainMenuRandomSprite.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MainMenuRandomSprite : MonoBehaviour {
+
+	public List<Sprite> sprites;
+
+	public SpriteRenderer sr1;
+	public SpriteRenderer sr2;
+
+	// Use this for initialization
+	void Awake () {
+
+		int max = sprites.Count;
+
+		int a = Random.Range(0, max);
+		int b = a + Random.Range(0, max-1);
+
+		sr1.sprite = sprites[a];
+		sr2.sprite = sprites[b];
+	}
+
+
+
+}
