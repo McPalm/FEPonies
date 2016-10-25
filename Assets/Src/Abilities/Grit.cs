43ad4994c418e2e0a3de8e3d8a17726a72f6ee08
Palm <tauman.exe@gmail.com>
2016-10-25 13:43:29 +0200

    Added Grit

---
+++ Assets/Src/Abilities/Grit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Grit : Passive, IDefenceModifiers
+{
+	public override string Name
+	{
+		get
+		{
+			return "Grit";
+		}
+	}
+
+	public int Priority
+	{
+		get
+		{
+			return -10;
+			// grit should always always always be last
+		}
+	}
+
+	public void Test(DamageData dd)
+	{
+		Stats s = dd.target.GetStatsAt(dd.target.Tile);
+		if (dd.ApplyDefences(s.defense, s.resistance) >= dd.target.CurrentHP)
+		{
+			dd.damageMultipler *= 0.5f;
+		}
+	}
+}
