e344587641edcb2959ef7d7221c0263a317bb342
Palm <tauman.exe@gmail.com>
2016-10-27 13:11:13 +0200

    Remade Sing to be more Current

---
+++ Assets/Src/Abilities/SingBuff.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class SingBuff : MonoBehaviour, IAttackModifier
+{
+	Sing artist;
+
+	public int Priority
+	{
+		get
+		{
+			return 0;
+		}
+	}
+
+	public void Test(DamageData dd)
+	{
+		if (IsActive && WithinRange(dd.SourceTile))
+			dd.damageMultipler *= 1.15f;
+	}
+
+	public void Initialize(Sing artist)
+	{
+		this.artist = artist;
+	}
+
+	bool WithinRange(Component location)
+	{
+		return TileGrid.GetDelta(location, artist) < 4;
+	}
+
+	bool IsActive
+	{
+		get
+		{
+			return artist.Active;
+		}
+	}
+}
