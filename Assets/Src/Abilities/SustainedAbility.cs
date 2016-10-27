d0e58ce8f62b78f1e9f1783d243a3c4b8a62ccaf
Palm <tauman.exe@gmail.com>
2016-10-27 11:35:39 +0200

    Forgot to add

---
+++ Assets/Src/Abilities/SustainedAbility.cs
@@ -0,0 +1,9 @@
+﻿/// <summary>
+/// Use this interface on abilites that are sustainer or have a duration
+/// Used by the GUI to know whenever ot not to display ab ability being active.
+/// </summary>
+public interface SustainedAbility {
+
+	bool Active { get; }
+
+}
