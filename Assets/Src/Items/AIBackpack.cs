c9245614020bf8d746f7084f7ff357d57153dd0b
Peter Åkesson <petak86@gmail.com>
2016-11-03 16:21:06 +0100

    AIBackpack don't trigger if you already have a backpack

--- Assets/Src/Items/AIBackpack.cs
+++ Assets/Src/Items/AIBackpack.cs
@@ -15,30 +15,33 @@ public class AIBackpack : MonoBehaviour {
 	void Start()
 	{
 		Character c = GetComponent<Unit>().Character;
-		int level = c.Level;
-		int budget = level;
-		if (armor != "")
-		{
-			Armor a = ArmorDB.Instance.GetArmor(armor, budget);
-			if (a == null) Debug.LogError(armor + " not found in armor database");
-			c.Backpack.Add(a);
-			c.Backpack.Equip(a);
-			budget += level - ArmorDB.Instance.GetLevel(a.Name);
-		}
-		else budget += 2;
+        if (c.Backpack == null)
+        {
+            int level = c.Level;
+            int budget = level;
+            if (armor != "")
+            {
+                Armor a = ArmorDB.Instance.GetArmor(armor, budget);
+                if (a == null) Debug.LogError(armor + " not found in armor database");
+                c.Backpack.Add(a);
+                c.Backpack.Equip(a);
+                budget += level - ArmorDB.Instance.GetLevel(a.Name);
+            }
+            else budget += 2;
 
-		if (weapon != "")
-		{
-			Weapon w = WeaponDB.Instance.GetWeapon(weapon, budget);
-			if (w == null) Debug.LogError(weapon + " not found in weapons database");
-			c.Backpack.Add(w);
-			c.Backpack.Equip(w);
-		}
-		foreach (string e in extras)
-		{
-			Item i = ItemFactory.CreateItem(e);
-			if (i.Name == "Rubbish") Debug.LogError(e + " generated Rubbish.");
-			if (!c.Backpack.Add(i)) Debug.LogWarning(c.Name + " ran out of inventory space.");
-		}
+            if (weapon != "")
+            {
+                Weapon w = WeaponDB.Instance.GetWeapon(weapon, budget);
+                if (w == null) Debug.LogError(weapon + " not found in weapons database");
+                c.Backpack.Add(w);
+                c.Backpack.Equip(w);
+            }
+            foreach (string e in extras)
+            {
+                Item i = ItemFactory.CreateItem(e);
+                if (i.Name == "Rubbish") Debug.LogError(e + " generated Rubbish.");
+                if (!c.Backpack.Add(i)) Debug.LogWarning(c.Name + " ran out of inventory space.");
+            }
+        }
 	}
 }
