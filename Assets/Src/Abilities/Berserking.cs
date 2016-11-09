012e8f20560a278a3d1f29ee3858d91757c23746
Palm <tauman.exe@gmail.com>
2016-11-09 09:58:49 +0100

    Created Berserking skill

---
+++ Assets/Src/Abilities/Berserking.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Berserking : Passive, IAttackModifier {
+
+	bool active = false;
+
+	public override string Name
+	{
+		get
+		{
+			return "Berserking";
+		}
+	}
+
+	public int Priority
+	{
+		get
+		{
+			return 0;
+		}
+	}
+
+	public void Test(DamageData dd)
+	{
+		if(active)
+		{
+			dd.damageMultipler *= 1.2f;
+		}
+	}
+
+	void FinishedAttackSequence(Unit u)
+	{
+		Unit user = GetComponent<Unit>();
+		if (user.damageTaken >= user.CurrentHP)
+		{
+			Activate();
+		}
+	}
+
+	void Activate()
+	{
+		active = true;
+		GetComponent<SpriteRenderer>().color = Color.red;
+	}
+}
