d86a1fca91df4a0844f44272ac4b3a3b54e74937
Palm <tauman.exe@gmail.com>
2016-11-09 15:26:03 +0100

    Added DirtyUpdate, used to get access to the Update loop outside of Monobehaviours

---
+++ Assets/Src/DirtyUpdate.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class DirtyUpdate : MonoBehaviour {
+
+	static DirtyUpdate _instance;
+
+	List<System.Action> _update;
+	List<System.Action> _removeUpdate;
+
+	static public DirtyUpdate Instance
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				new GameObject().AddComponent<DirtyUpdate>();
+			}
+			return _instance;
+		}
+	}
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		_instance = this;
+		name = "DirtyUpdate";
+		_update = new List<System.Action>();
+		_removeUpdate = new List<System.Action>();
+	}
+
+	public void RegisterUpdate(System.Action a)
+	{
+		_update.Add(a);
+	}
+
+	public void UnregisterUpdate(System.Action a)
+	{
+		_removeUpdate.Add(a);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		foreach (System.Action a in _update)
+		{
+			a();
+		}
+		foreach(System.Action a  in _removeUpdate)
+		{
+			_update.Remove(a);
+		}
+		_removeUpdate.Clear();
+	}
+}
