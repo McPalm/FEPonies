6eed7f7eb349e694beef4004cdc67f83a870828c
Palm <tauman.exe@gmail.com>
2016-11-15 15:54:31 +0100

    Added Gates!

---
+++ Assets/Switch.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Switch : MonoBehaviour, IDefenceModifiers
+{
+	public TileGate[] _gates;
+
+	public int Priority
+	{
+		get
+		{
+			return 0;
+		}
+	}
+
+	void Start()
+	{
+		Weapon w = new Weapon();
+		w.attackInfo = new AttackInfo(new Norange());
+		Backpack b = GetComponent<Unit>().Character.Backpack;
+
+		b.Add(w);
+		b.Equip(w);
+	}
+
+	public void DefenceTest(DamageData dd)
+	{
+		dd.damageMultipler = 0f;
+		if (!dd.testAttack) OpenClose();
+	}
+
+	void OpenClose()
+	{
+		foreach(TileGate g in _gates)
+		{
+			g.Open = !g.Open;
+		}
+			
+	}
+}
---
+++ Assets/TileGate.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class TileGate : Tile {
+
+	public GameObject gateThingy;
+
+	bool _open;
+
+	public bool Open
+	{
+		get { return _open; }
+		set {
+			_open = value;
+			if (_open) gateThingy.SetActive(false);
+			else gateThingy.SetActive(true);
+		}
+	}
+	public override TileType Type
+	{
+		get
+		{
+			return (_open) ? TileType.regular : TileType.wall;
+		}
+	}
+	public override bool ShootThrough
+	{
+		get
+		{
+			return _open;
+		}
+	}
+	public override void GetReachableTiles(UnitMove move, HashSet<Tile> reachTiles, Unit team)
+	{
+		if (_open) base.GetReachableTiles(move, reachTiles, team);
+		return;
+	}
+}
